a858664100a4c28ab617c849b09b754660cff189
Cristian Lozano <criscalovis@gmail.com>
2016-09-03 23:00:04 -0500

    fix iptables command for grateful haproxy reload

--- reload.sh
+++ reload.sh
@@ -3,4 +3,4 @@ iptables -I INPUT -p tcp --dport 443 --syn -j DROP
 sleep 1
 supervisorctl restart haproxy
 iptables -D INPUT -p tcp --dport 80 --syn -j DROP
-iptables -I INPUT -p tcp --dport 443 --syn -j DROP
+iptables -D INPUT -p tcp --dport 443 --syn -j DROP
